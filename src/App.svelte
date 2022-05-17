bddaf50733896117d743ab2a55c3a9a5b6714c8a
Satont <satontworldwide@gmail.com>
2022-05-18 00:51:09 +0300

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+
+  import { Router, Route } from "svelte-navigator";
+  import Main from './pages/Main.svelte'
+  import Dashboard from './pages/Dashboard.svelte'
+</script>
+
+<h1 class="text-3xl font-bold underline">
+  Hello world!
+</h1>
+
+<Router>
+  <Route path="dashboard/*">
+    <Route path="/" component={Dashboard} />
+  </Route>
+
+  <Route component={Main} />
+</Router>
+
+<style>
+
+</style>
